91faa78c65c99768dd55bcbbf6bf6c93deb85ae2
EsamMohamedSrour <88456621+EsamMohamedSrour@users.noreply.github.com>
2021-08-05 23:40:40 +0200

    Update LCD.c
    plz change ( void LCD_WriteData  ) to (void LCD_WriteChar  ) as in the lcd.h  file

--- HAL/LCD/LCD.c
+++ HAL/LCD/LCD.c
@@ -22,7 +22,15 @@ void LCD_Init(void)
 
 void LCD_WriteData(UINT8 Data)
 {
-	
+	GPIO_WRITE_PIN_VALUE(RS_PORT,RS_PIN,1 );  //putting 1 on rs pin so i can write data on lcd 
+	GPIO_WRITE_PIN_VALUE(RW_PORT,RW_PIN,0);   
+	GPIO_WRITE_PIN_VALUE(EN_PORT,EN_PIN,1);
+
+	GPIO_WRITE_PORT_VALUE(LCD_PORT , Data) ;
+	GPIO_WRITE_PIN_VALUE(EN_PORT,EN_PIN,1);
+	_delay_ms(2);                //  i write this  #include <util/delay.h>    so we can use delay function.... 
+	GPIO_WRITE_PIN_VALUE(EN_PORT,EN_PIN,0);
+	_delay_ms(2);    
 }
 
 
