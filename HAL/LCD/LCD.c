00c4db270f211627e7feb0e409a1129b67a7ba98
IslamMohamed8 <79092948+IslamMohamed8@users.noreply.github.com>
2021-08-05 21:10:37 +0200

    Update LCD.c

--- HAL/LCD/LCD.c
+++ HAL/LCD/LCD.c
@@ -1,17 +1,16 @@
 #include "LCD.h"
+#include "../../MCAL/GPIO/GPIO_CONFIG.h"
 #include <util/delay.h>  // so i can use _dealy_ms() function
-
-
 
 
 static void LCD_WriteCommand(UINT8 Command)
 {
-	GPIO_WRITE_PIN_VALUE(RS_PORT, RS_PIN, 0);  // so i can write command
-	GPIO_WRITE_PIN_VALUE(RW_PORT, RW_PIN, 0); //  to tell the lcd that i am going to write data
+	GPIO_WRITE_PIN_VALUE(RS_PORT, RS_PIN, LOW);  // so i can write command
+	GPIO_WRITE_PIN_VALUE(RW_PORT, RW_PIN, LOW); //  to tell the lcd that i am going to write data
 	GPIO_WRITE_PORT_VALUE(LCD_PORT, Command); // passing the command through the lcd_port
-	GPIO_WRITE_PIN_VALUE(EN_PORT, EN_PIN, 1); // activating the enable
+	GPIO_WRITE_PIN_VALUE(EN_PORT, EN_PIN, HIGH); // activating the enable
 	_delay_ms(2);
-	GPIO_WRITE_PIN_VALUE(EN_PORT, EN_PIN, 0); // disabling the enable
+	GPIO_WRITE_PIN_VALUE(EN_PORT, EN_PIN, LOW); // disabling the enable
 	_delay_ms(2);
 }
 
