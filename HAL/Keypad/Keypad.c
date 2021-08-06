96c22e71511369fb702ad9af3c0c2c6e3f76ac97
YousefAdelElaraby <88454498+YousefAdelElaraby@users.noreply.github.com>
2021-08-06 22:28:29 +0200

    Update Keypad.c

--- HAL/Keypad/Keypad.c
+++ HAL/Keypad/Keypad.c
@@ -7,18 +7,20 @@ static const UINT8 keypad_array[4][4] =  { KEYPAD_ARRAY };
 
 void Keypad_Init(void){
   
-  int row [4] = {R1_PIN, R2_PIN, R3_PIN, R4_PIN};// so i can loop in row pins instead of writing multiple lines
-	int coloumb [4] = {C1_PIN, C2_PIN, C3_PIN, C4_PIN};// so i can loop in coloumb pins instead of writing multiple lines
-	int i ;
-	for (i=0; i<4; i++)
-	{
-		GPIO_INIT_PIN_DIRECTION(KEYPAD_PORT, row[i], OUTPUT);
-		GPIO_WRITE_PIN_VALUE(KEYPAD_PORT, row[i], type_of_connection);
-	}
-	for (i=0; i<4; i++)
-	{
-		GPIO_INIT_PIN_DIRECTION(KEYPAD_PORT, coloumb[i], INPUT);
-	}
+	GPIO_INIT_PIN_DIRECTION(KEYPAD_PORT, R1_PIN, OUTPUT);
+	GPIO_INIT_PIN_DIRECTION(KEYPAD_PORT, R2_PIN, OUTPUT);
+	GPIO_INIT_PIN_DIRECTION(KEYPAD_PORT, R3_PIN, OUTPUT);
+	GPIO_INIT_PIN_DIRECTION(KEYPAD_PORT, R4_PIN, OUTPUT);
+
+	GPIO_WRITE_PIN_VALUE(KEYPAD_PORT, R1_PIN, type_of_connection);
+	GPIO_WRITE_PIN_VALUE(KEYPAD_PORT, R2_PIN, type_of_connection);
+	GPIO_WRITE_PIN_VALUE(KEYPAD_PORT, R3_PIN, type_of_connection);
+	GPIO_WRITE_PIN_VALUE(KEYPAD_PORT, R4_PIN, type_of_connection);
+
+	GPIO_INIT_PIN_DIRECTION(KEYPAD_PORT, C1_PIN, INPUT);
+	GPIO_INIT_PIN_DIRECTION(KEYPAD_PORT, C2_PIN, INPUT);
+	GPIO_INIT_PIN_DIRECTION(KEYPAD_PORT, C3_PIN, INPUT);
+	GPIO_INIT_PIN_DIRECTION(KEYPAD_PORT, C4_PIN, INPUT);
 
 }
 
