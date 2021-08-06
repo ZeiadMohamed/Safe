e0e5a0b2583f836489ed3a935d229cdc8c5a39ca
IslamMohamed8 <79092948+IslamMohamed8@users.noreply.github.com>
2021-08-06 16:43:25 +0200

    Add files via upload

---
+++ HAL/Keypad/Keypad.c
@@ -0,0 +1,18 @@
+#include "Keypad.h"
+#include "Keypad_Config.h"
+
+
+static const UINT8 keypad_array[4][4] =  { KEYPAD_ARRAY };
+
+
+void Keypad_Init(void){
+
+
+}
+
+
+UINT8 Keypad_GetKey(void){
+
+
+
+}
---
+++ HAL/Keypad/Keypad_Config.h
@@ -0,0 +1,30 @@
+#ifndef __KEYPAD_CONFIG_H__
+#define __KEYPAD_CONFIG_H__
+
+#include "../../MCAL/GPIO/GPIO_CONFIG.h"
+/******************************************************************
+*                       Pins &  Keypad Symbols                    *
+******************************************************************/
+
+#define KEYPAD_ARRAY   {'7','8','9','D'},\
+				       {'4','5','6','M'},\
+				       {'1','2','3','N'},\
+				       {'C','0','=','P'}
+
+
+#define KEYPAD_PORT PORT_B
+#define R1_PIN 0
+#define R2_PIN 1
+#define R3_PIN 2
+#define R4_PIN 3
+#define C1_PIN 4
+#define C2_PIN 5
+#define C3_PIN 6
+#define C4_PIN 7
+
+
+
+
+
+
+#endif
