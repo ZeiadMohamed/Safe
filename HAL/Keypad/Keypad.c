313d8e98f4597b54892d4910ce45541f4adda25d
EsamMohamedSrour <88456621+EsamMohamedSrour@users.noreply.github.com>
2021-08-06 19:17:29 +0200

    Update Keypad.c
     Eslam check this  for any error plz @IslamMohamed8

--- HAL/Keypad/Keypad.c
+++ HAL/Keypad/Keypad.c
@@ -2,7 +2,7 @@
 #include "Keypad_Config.h"
 
 
-static const UINT8 keypad_array[4][4] =  { KEYPAD_ARRAY };
+static const UINT8 keypad_array[COL][ROW] =  { KEYPAD_ARRAY };   //more readable
 
 
 void Keypad_Init(void){
@@ -11,8 +11,31 @@ void Keypad_Init(void){
 }
 
 
-UINT8 Keypad_GetKey(void){
+UINT8 Keypad_GetKey(void)
+{
+  UINT8 ROWCOUNT;
+	UINT8 COLCOUNT;
+	UINT8 RESULT;
+	for(ROWCOUNT=ROW_INT; ROWCOUNT<ROW_END ;ROWCOUNT++)
+	{
+		GPIO_WRITE_PIN_VALUE(PORT_B ,ROWCOUNT , LOW);
+		for (COLCOUNT=COL_INT ;COLCOUNT<COL_END ;COLCOUNT++)
+		{
+			if(GPIO_READ_PIN_VALUE(PORT_B,COLCOUNT)==LOW)
+			{
+				RESULT=keypad_array[ROWCOUNT][COLCOUNT-COL_INT];
+				 /* because we don't use timer we use this condition for one press */
+				while(GPIO_READ_PIN_VALUE(PORT_B,COLCOUNT)==LOW);
+				
+				_delay_ms(10);  //to avoid bouncing
+			}
+			
+		}
+    
+		GPIO_WRITE_PIN_VALUE(PORT_B ,ROWCOUNT , HIGH);  //deactivate again
+	}
 
+   return RESULT;
 
 
 }
