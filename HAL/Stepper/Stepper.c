d8c3ada7b306cc0515552cc9bc701059e45af9ce
YousefAdelElaraby <88454498+YousefAdelElaraby@users.noreply.github.com>
2021-08-08 19:17:38 +0200

    Update Stepper.c

--- HAL/Stepper/Stepper.c
+++ HAL/Stepper/Stepper.c
@@ -3,6 +3,7 @@ static UINT8 DELAY_TIME = 100;// to be able to change time in (ms)
 
 
 
+
 void Stepper_Init(){
 
 
@@ -10,12 +11,12 @@ void Stepper_Init(){
 }
 
 
-void Stepper_SetSpeed(UINT8 speed){
+void Stepper_SetSpeed(UINT8 speed)
+{
+	DELAY_TIME = (1/((STEPS_PER_REVOLUTION * speed)/60))*1000 ; // time in ms
 
-  	speed *= 60;
-	DELAY_TIME = RESOLUTION/speed ;
+
 }
-
 
 void Stepper_SetDirection(UINT8 direction){
 
