d536539932165f2ad2e5653c6b515a396c2ec833
MennaAbdou <88550001+MennaAbdou@users.noreply.github.com>
2021-08-10 16:41:23 +0200

    Update Stepper.c

--- HAL/Stepper/Stepper.c
+++ HAL/Stepper/Stepper.c
@@ -1,11 +1,33 @@
 #include "Stepper.h"
 
 
+static UINT8 DELAY_TIME = 100;// to be able to change time in (ms)
+
+static UINT8 motor_direction = MOTOR_DIRECTION;
 
 
 
 void Stepper_Init(){
 
+#if MOTOR_TYPE == UNIPOLAR
+	 GPIO_INIT_PIN_DIRECTION(MOTOR_PORT ,WINDING_A, OUTPUT);
+	 GPIO_INIT_PIN_DIRECTION(MOTOR_PORT ,WINDING_B, OUTPUT);
+	 GPIO_INIT_PIN_DIRECTION(MOTOR_PORT ,WINDING_C, OUTPUT);
+	 GPIO_INIT_PIN_DIRECTION(MOTOR_PORT ,WINDING_D, OUTPUT);
+	 GPIO_INIT_PIN_DIRECTION(MOTOR_PORT ,WINDING_E, OUTPUT);
+	 GPIO_INIT_PIN_DIRECTION(MOTOR_PORT ,WINDING_F, OUTPUT);
+
+#elif MOTOR_TYPE == BIPOLAR
+	 GPIO_INIT_PIN_DIRECTION(MOTOR_PORT ,WINDING_A, OUTPUT);
+	 GPIO_INIT_PIN_DIRECTION(MOTOR_PORT ,WINDING_B, OUTPUT);
+	 GPIO_INIT_PIN_DIRECTION(MOTOR_PORT ,WINDING_C, OUTPUT);
+	 GPIO_INIT_PIN_DIRECTION(MOTOR_PORT ,WINDING_D, OUTPUT);
+
+#elif MOTOR_TYPE == UNIVERSAL
+
+	GPIO_INIT_PORT_DIRECTION(MOTOR_PORT,OUTPUT);
+
+#endif
 
 
 }
@@ -13,18 +35,113 @@ void Stepper_Init(){
 
 void Stepper_SetSpeed(UINT8 speed){
 
+DELAY_TIME = (1/((STEPS_PER_REVOLUTION * speed)/60))*1000 ; // time in ms
 
 }
 
 
 void Stepper_SetDirection(UINT8 direction){
-
+switch(direction){
+	case CLOCKWISE:
+		motor_direction = CLOCKWISE;
+		break;
+	case COUNTER_CLOCKWISE:
+		motor_direction = COUNTER_CLOCKWISE;
+		break;
+	}
 
 }
 
 
 void Stepper_Step(UINT8 steps){
 
+void stepper_half_Revolution(void)
+{
+        UINT8 i=0;
+        UNIT8 j
+	PORT_D=0x00; 
+
+
+	while(1)
 
 }
 
+//Half Revolution clockwise
+  
+  for(j=0;j<24;j++)
+    
+  {
+    
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT0 , UINT8 1)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT1 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT2 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT3 , UINT8 0)
+          
+          _delay_ms(10);
+        
+     
+         GPIO_WRITE_PORT_VALUE(UINT8 PORT0 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT1 , UINT8 1)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT2 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT3 , UINT8 0)
+          
+          _delay_ms(10);
+         GPIO_WRITE_PORT_VALUE(UINT8 PORT0 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT1 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT2 , UINT8 1)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT3 , UINT8 0)
+          
+          _delay_ms(10);
+         GPIO_WRITE_PORT_VALUE(UINT8 PORT0 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT1 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT2 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT3 , UINT8 1)
+          
+          _delay_ms(10);
+    
+           if(i==4)
+			    	i=0;
+          
+  }
+  
+  
+  //Rotate counter clockwise
+  
+  
+  for(j=0;j<24;j++)
+    
+  {
+    
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT0 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT1 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT2 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT3 , UINT8 1)
+          
+          _delay_ms(10);
+        
+     
+         GPIO_WRITE_PORT_VALUE(UINT8 PORT0 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT1 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT2 , UINT8 1)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT3 , UINT8 0)
+          
+          _delay_ms(10);
+         GPIO_WRITE_PORT_VALUE(UINT8 PORT0 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT1 , UINT8 1)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT2 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT3 , UINT8 0)
+          
+          _delay_ms(10);
+         GPIO_WRITE_PORT_VALUE(UINT8 PORT0 , UINT8 1)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT1 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT2 , UINT8 0)
+        GPIO_WRITE_PORT_VALUE(UINT8 PORT3 , UINT8 0)
+          
+          _delay_ms(10);
+    
+         if(i==4)
+						i=0;
+          
+  }
+  
+}
