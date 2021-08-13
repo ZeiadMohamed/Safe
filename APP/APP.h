a4ce4a01a5507299bc7316095591b679f53c7e1f
IslamMohamed8 <79092948+IslamMohamed8@users.noreply.github.com>
2021-08-14 00:08:40 +0200

    Update APP.h

--- APP/APP.h
+++ APP/APP.h
@@ -1,35 +1,3 @@
-
-#ifndef APP_H_
-#define APP_H_
-
-#include "../LIB/STD_TYPES.h"
-
-
-/* 	 Function    : print_enter_new_password
-**   Parameters  : None
-**   Return      : None
-**   Description : It will clear LCD and print Enter New Pass
-*/
-void print_enter_new_password(void);
-
-
-/* 	 Function    : get_password
-**   Parameters  : None
-**   Return      : password
-**   Description : It will get key from user , print '*' on LCD
-*/
-UINT8 get_password(void);
-
-/* 	 Function    : print_enter_confirm_password
-**   Parameters  : None
-**   Return      : None
-**   Description : It will clear LCD and print Enter Confirmation Pass
-*/
-void print_enter_confirm_password(void);
-
-
-#endif /* APP_H_ */
-=======
 #ifndef APP_H_
 #define APP_H_
 
@@ -69,6 +37,14 @@ void print_enter_old_password(void);
 **   Description : It will clear LCD and print " Enter master Password "
 */
 void print_enter_master_password(void);
+
+
+/* 	 Function    : print_enter_confirm_password
+**   Parameters  : None
+**   Return      : None
+**   Description : It will clear LCD and print Enter Confirmation Pass
+*/
+void print_enter_confirm_password(void);
 
 
 
