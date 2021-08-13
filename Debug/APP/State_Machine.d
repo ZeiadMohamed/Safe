570c86bb61bc1ad5ca135aa87a6cdce1e93469c6
Abdallah Nasser <88454178+Tblawy@users.noreply.github.com>
2021-08-13 23:38:53 +0200

    Add files via upload

---
+++ Debug/APP/State_Machine.d
@@ -0,0 +1,8 @@
+APP/State_Machine.d APP/State_Machine.o: ../APP/State_Machine.c \
+  ../APP/State_Machine.h ../APP/APP.h ../APP/../LIB/STD_TYPES.h
+
+../APP/State_Machine.h:
+
+../APP/APP.h:
+
+../APP/../LIB/STD_TYPES.h:
---
+++ Debug/HAL/LCD/LCD.d
@@ -0,0 +1,19 @@
+HAL/LCD/LCD.d HAL/LCD/LCD.o: ../HAL/LCD/LCD.c ../HAL/LCD/LCD.h \
+  ../HAL/LCD/LCD_CONF.h ../HAL/LCD/../../MCAL/GPIO/GPIO_CONFIG.h \
+  ../HAL/LCD/../../MCAL/GPIO/GPIO.h \
+  ../HAL/LCD/../../MCAL/GPIO/../../LIB/STD_TYPES.h \
+  ../HAL/LCD/../../LIB/STD_TYPES.h ../HAL/LCD/../../LIB/UTILES.h
+
+../HAL/LCD/LCD.h:
+
+../HAL/LCD/LCD_CONF.h:
+
+../HAL/LCD/../../MCAL/GPIO/GPIO_CONFIG.h:
+
+../HAL/LCD/../../MCAL/GPIO/GPIO.h:
+
+../HAL/LCD/../../MCAL/GPIO/../../LIB/STD_TYPES.h:
+
+../HAL/LCD/../../LIB/STD_TYPES.h:
+
+../HAL/LCD/../../LIB/UTILES.h:
