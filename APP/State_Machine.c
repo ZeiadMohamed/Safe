53bd58ae36a6305ca42332018fc920ab02dcd709
Harb <88456932+ShehabAhmedAdel@users.noreply.github.com>
2021-08-14 02:13:11 +0200

    Update State_Machine.c

--- APP/State_Machine.c
+++ APP/State_Machine.c
@@ -121,10 +121,14 @@ void state_machine(void){
 
 			break;
 		case OPEN_SAFE:
-
+			open_safe();
+			wait_in_second(3);
+			state = CLOSE_SAFE;
 		break;
+			
 		case CLOSE_SAFE:
-
+	                close_safe();
+			state = ENTER_PASSWORD;
 		break;
 		
 
