cc2c6ee43ba6328952c39acc5f0354a8ce000b55
Harb <88456932+ShehabAhmedAdel@users.noreply.github.com>
2021-08-14 02:11:25 +0200

    Update APP.h

--- APP/APP.h
+++ APP/APP.h
@@ -63,6 +63,29 @@ void print_enter_password(void);
 UINT8 key_or_pass(void);
 
 
+/* 	 Function    : open_safe
+**   Parameters  : None
+**   Return      : None
+**   Description : It will determination of motor direction and open the safe
+*/
+void open_safe(void);
+
+
+/* 	 Function    : close_safe
+**   Parameters  : None
+**   Return      : None
+**   Description : It will close the safe
+*/
+void close_safe(void);
+
+
+/* 	 Function    : wait_in_second
+**   Parameters  : time
+**   Return      : None
+**   Description : It will wait for user enter seconds
+*/
+void wait_in_second(UINT8 time);
+
 
 #endif /* APP_H_ */
 
